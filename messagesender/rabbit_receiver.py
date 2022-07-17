905acc50cb33be6d5e9e0222e7a691a2c07924e7
Peer-Lucas Jeske <lucas.jeske@posteo.de>
2022-07-17 08:28:10 +0200

    logging fix

--- messagesender/rabbit_receiver.py
+++ messagesender/rabbit_receiver.py
@@ -11,17 +11,22 @@ log = logging.getLogger('rabbit_receiver')
 
 
 def callback(ch, method, properties, body):
+    log.debug("Received message: %s", body)
     print("Got message: %s", body)
     try:
         message = json.loads(body)
     except json.decoder.JSONDecodeError:
+        log.debug("Invalid JSON message received: %s", body)
         print("Invalid JSON message received: %s" % body)
         return
     params = message['params']
     url = message['url']
 
     print("Sending received message: %s", message)
+    log.debug("Sending received message: %s", message)
     resp = requests.post(url, params=params)
+    log.debug("Got response (%i): %s", resp.status_code, resp.content)
+    log.debug("Message sent. Sleeping for 1 second.")
     print("Got response (%i): %s", resp.status_code, resp.content)
     print("Message sent. Sleeping for 1 second.")
     time.sleep(1.5)
@@ -44,6 +49,7 @@ def wait_for_rabbitmq(host: str, port: int, timeout=3):
 
 if __name__ == '__main__':
     config = yaml.safe_load(open('config.yaml', encoding="utf-8"))
+    log.setLevel(logging.DEBUG)
     if config is None or config.get('rabbitmq') is None:
         url = "amqp://rabbitmq:rabbitmq@rabbitmq:5672/"
         host = 'localhost'
@@ -54,9 +60,12 @@ if __name__ == '__main__':
         host = config['rabbitmq']['host']
         # user = config['rabbitmq']['user']
         # password = config['rabbitmq']['password']
-    # print(f"Connecting to RabbitMQ on {host}, with user {user} and password {password}")
+    print(f"Connecting to RabbitMQ on {host}")
+    log.debug("Connecting to RabbitMQ on %s", host)
     wait_for_rabbitmq(host, 5672)
     params = pika.URLParameters(url)
+    print(f"Connecting to RabbitMQ on url {url}")
+    log.debug("Connecting to RabbitMQ on url %s", url)
     connection = pika.BlockingConnection(params)
     # credentials = pika.PlainCredentials(username=user, password=password)
     # pika_params = pika.ConnectionParameters(host=host, port=5672, credentials=credentials)
@@ -66,6 +75,7 @@ if __name__ == '__main__':
     channel.basic_consume(queue='telegram', auto_ack=True, on_message_callback=callback)
     try:
         print("Waiting for messages...")
+        log.debug("Waiting for messages...")
         channel.start_consuming()
     except KeyboardInterrupt:
         channel.stop_consuming()
